898847c383e6988b989b67fbe611eaf7ac1d1d41
priyanka Magdum <pmagdum>
2016-08-06 16:25:43 +0530

    loading JenkinsDemo..

---
+++ src/JenkinsPkg/Demo.java
@@ -0,0 +1,20 @@
+package JenkinsPkg;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo 
+{
+	@Test
+	public void testA()
+	{
+		System.setProperty("webdriver.gecko.driver","C:/Users/CBT/Downloads/geckodriver-v0.9.0-win64/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("Https://www.google.com");
+		Reporter.log("Opening google..",true);
+		//driver.close();
+	}
+
+} 
